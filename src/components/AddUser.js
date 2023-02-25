b68f8bf8882ebcaa5b7a42b0d3cbfd5c3f2a2da5
Nikita Kushnirov <niki.cushnirov@yandex.ru>
2023-02-25 16:46:23 +0300

    git init

---
+++ src/components/AddUser.js
@@ -0,0 +1,43 @@
+import React from "react"
+
+class AddUser extends React.Component {
+    userAdd = {}
+    constructor(props) {
+        super(props)
+        this.state = {
+            firstname: "",
+            lastname: "",
+            bio: "",
+            age: 0,
+            isHappy: false
+        }
+    }
+    render() {
+        return (
+            <form ref={(form) => this.myForm = form} >
+                <input placeholder="Имя" onChange={(event) => this.setState({firstname: event.target.value})}/>
+                <input placeholder="Фамилия" onChange={(event) => this.setState({lastname: event.target.value})} />
+                <textarea placeholder="Биография" onChange={(event) => this.setState({bio: event.target.value})}></textarea>
+                <input placeholder="Возраст" onChange={(event) => this.setState({age: event.target.value})}/>
+                <label htmlFor="isHappy" >Счастлив?</label>
+                <input type="checkbox" id="isHappy" onChange={(event) => this.setState({isHappy: event.target.checked})}/>
+                <button type="button" onClick={() => {
+                    this.myForm.reset()
+                    this.userAdd = {
+                        firstname: this.state.firstname,
+                        lastname: this.state.lastname,
+                        bio: this.state.bio,
+                        age: this.state.age,
+                        isHappy: this.state.isHappy
+                    }
+                    if(this.props.user) {
+                        this.userAdd.id = this.props.user.id
+                    }
+                    this.props.onAdd(this.userAdd)
+                }}>Добавить</button>
+            </form>
+        )
+    }
+}
+
+export default AddUser
